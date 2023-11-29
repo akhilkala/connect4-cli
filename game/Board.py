812741636cec6fb5661ea1d64331ba20048e1a9c
Akhil Kala <kalaakki@gmail.com>
2023-11-29 20:27:29 +0530

    added board

--- game/Board.py
+++ game/Board.py
@@ -1,2 +1,45 @@
 class Board:
-    pass
+    _EMPTY_SYMBOL = "_"
+
+    def __init__(self, board_size):
+        self._board = self._create(board_size)
+        self.number_of_rows = board_size[0]
+        self.number_of_cols = board_size[1]
+
+    def _create(self, board_size):
+        rows = board_size[0]
+        columns = board_size[1]
+        board = []
+        for col in range(columns):
+            col = []
+            for _ in range(rows):
+                col.append(Board._EMPTY_SYMBOL)
+            board.append(col)
+        return board
+
+    def available_columns(self):
+        cols = []
+        number_of_rows = len(self._board[0])
+        for i, col in enumerate(self._board):
+            col_size = len(list(filter((Board._EMPTY_SYMBOL).__ne__, col)))
+            if col_size != number_of_rows:
+                cols.append(i)
+        return cols
+
+    def placeInColumn(self, index, symbol):
+        rowIndex = -1
+        for i, element in enumerate(self._board[index]):
+            if element == Board._EMPTY_SYMBOL:
+                self._board[index][i] = symbol
+                rowIndex = i
+                break
+        return rowIndex
+
+    def get(self, col, row):
+        if col < 0 or col >= self.number_of_cols or row < 0 or row >= self.number_of_rows:
+            return None
+        return self._board[col][row]
+
+    def print(self):
+        print('\n'.join(['\t'.join([str(cell) for cell in row])
+              for row in self._board]))
--- game/Game.py
+++ game/Game.py
@@ -1,4 +1,4 @@
-from game.Player import *
+from game.Board import Board
 
 #  0  1  2  3  4  5  6
 # [ ][ ][ ][ ][ ][ ][ ]
@@ -10,30 +10,14 @@ from game.Player import *
 
 
 class Game:
-    _EMPTY_SYMBOL = "_"
-
     def __init__(self, board_size, player_a, player_b):
-        self.board = self._create_board(board_size)
+        self.board = Board(board_size)
         self.player_a = player_a
         self.player_b = player_b
         self.player_a_turn = True
 
-    def _create_board(self, board_size):
-        rows = board_size[0]
-        columns = board_size[1]
-        board = []
-        for col in range(columns):
-            col = []
-            for _ in range(rows):
-                col.append(Game._EMPTY_SYMBOL)
-            board.append(col)
-        return board
-
     # Gayatri im writing the other way
     def _check_win(self, last_played_pos):
-        number_of_cols = len(self.board)
-        number_of_rows = len(self.board[0])
-
         c = last_played_pos[0]
         r = last_played_pos[1]
         b = self.board
@@ -41,35 +25,35 @@ class Game:
         # does not work yet for instances where last played coin in not in at the end of the 4
 
         # up from last played coin
-        if c + 3 < number_of_cols and (b[c][r] == b[c+1][r] == b[c+2][r] == b[c+3][r]):
+        if b.get(c, r) == b.get(c+1, r) == b.get(c+2, r) == b.get(c+3, r):
             return True
 
         # down from last played coin
-        if c - 3 >= 0 and (b[c][r] == b[c-1][r] == b[c-2][r] == b[c-3][r]):
+        if b.get(c, r) == b.get(c-1, r) == b.get(c-2, r) == b.get(c-3, r):
             return True
 
         # right from last played coin
-        if r + 3 < number_of_rows and (b[c][r] == b[c][r+1] == b[c][r+2] == b[c][r+3]):
+        if b.get(c, r) == b.get(c, r+1) == b.get(c, r+2) == b.get(c, r+3):
             return True
 
         # left from last played coin
-        if r - 3 >= 0 and (b[c][r] == b[c][r-1] == b[c][r-2] == b[c][r-3]):
+        if b.get(c, r) == b.get(c, r-1) == b.get(c, r-2) == b.get(c, r-3):
             return True
 
         # top-right from last played coin
-        if c + 3 < number_of_cols and r + 3 < number_of_rows and (b[c][r] == b[c+1][r+1] == b[c+2][r+2] == b[c+3][r+3]):
+        if b.get(c, r) == b.get(c+1, r+1) == b.get(c+2, r+2) == b.get(c+3, r+3):
             return True
 
         # bottom-right from last played coin
-        if c + 3 < number_of_cols and r - 3 >= 0 and (b[c][r] == b[c+1][r-1] == b[c+2][r-2] == b[c+3][r-3]):
+        if b.get(c, r) == b.get(c+1, r-1) == b.get(c+2, r-2) == b.get(c+3, r-3):
             return True
 
         # top-left from last played coin
-        if c - 3 >= 0 and r + 3 < number_of_rows and (b[c][r] == b[c-1][r+1] == b[c-2][r+2] == b[c-3][r+3]):
+        if b.get(c, r) == b.get(c-1, r+1) == b.get(c-2, r+2) == b.get(c-3, r+3):
             return True
 
         # bottom-left from last played coin
-        if c - 3 >= 0 and r - 3 >= 0 and (b[c][r] == b[c-1][r-1] == b[c-2][r-2] == b[c-3][r-3]):
+        if b.get(c, r) == b.get(c-1, r-1) == b.get(c-2, r-2) == b.get(c-3, r-3):
             return True
 
         return False
@@ -77,29 +61,14 @@ class Game:
     def current_player(self):
         return self.player_a if self.player_a_turn else self.player_b
 
-    def available_columns(self):
-        cols = []
-        number_of_rows = len(self.board[0])
-        for i, col in enumerate(self.board):
-            col_size = len(list(filter((Game._EMPTY_SYMBOL).__ne__, col)))
-            if col_size != number_of_rows:
-                cols.append(i)
-        return cols
-
     def play(self, columnIndex, player):
-        if columnIndex not in self.available_columns():
+        if columnIndex not in self.board.available_columns():
             return
 
-        rowIndex = -1
-        for i, element in enumerate(self.board[columnIndex]):
-            if element == Game._EMPTY_SYMBOL:
-                self.board[columnIndex][i] = player.symbol
-                rowIndex = i
-                break
+        rowIndex = self.board.placeInColumn(columnIndex, player.symbol)
         win = self._check_win([columnIndex, rowIndex])
         self.player_a_turn = not self.player_a_turn
         return win
 
     def print(self):
-        print('\n'.join(['\t'.join([str(cell) for cell in row])
-              for row in self.board]))
+        self.board.print()
